53da0fefe721286adb98c13eaf240a72e5cbf9ef
MrJeleika <den932005@gmail.com>
2025-10-29 20:50:03 +0200

    fix: add buffer

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
 import '@xyflow/react/dist/style.css'
+import { Buffer } from 'buffer'
+
+window.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
+        buffer: 'buffer',
       },
     },
   }
